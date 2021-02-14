d0ee3f6e213557edc629a4c912146ca171798660
redpigzhu <zhuhong_1999@hotmail.com>
2021-02-13 21:31:37 -0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="cloth-view">
+        <div class="cloth-view__select">
+         <label for="selectCloth"><strong>select Cloth to view </strong></label>
+          <select id="selectCloth" v-model="selectedClothName" @change="changeSelection">
+              <option :value="acloth.name" v-for="(acloth, index) in user.cloths" :key="index">
+                  {{ acloth.name }}
+              </option>
+          </select>
+          </div>
+      <ThreedView :objPath="selectedCloth.src" :mtlPath="selectedCloth.mtl" :key="componentKey" />
+     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ThreedView from "./components/ThreedView";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  export default {
+    name: 'App',
+    components: { ThreedView },
+    data() {
+      return {
+        componentKey: 0,
+        selectedClothName: 'hoodie',
+        selectedCloth: {
+          src: "https://eddiekusel.github.io/hoodie.obj",
+          mtl : "https://eddiekusel.github.io/hoodie.mtl"
+        },
+        user:{
+          id:1,
+          username:'whoami',
+          firstName:"who",
+          lastName:'am',
+          email:'whoami@gmail.com',
+          isAdmin: true,
+          cloths :[
+          {id: 1, name: "hoodie"},
+          {id: 2, name: "knit"}
+        ]
+
+      }
+    }
+    },
+    methods: {
+      changeSelection()
+      {
+        this.selectedCloth.src = `https://eddiekusel.github.io/${this.selectedClothName}.obj`;
+        this.selectedCloth.mtl = `https://eddiekusel.github.io/${this.selectedClothName}.mtl`;
+        this.componentKey += 1;  
+        console.log(this.selectedCloth );
+      }
+    }
+  };
+  </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.cloth-view {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+  text-align: left;
 }
 </style>
---
+++ src/components/ThreedView.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>
+    <!-- <h2> {{objPath}} </h2> -->
+    <model-obj  :backgroundAlpha="0"
+        @on-load="onLoad" :src="objPath" :mtl="mtlPath" ></model-obj>
+   </div>
+</template>
+
+<script>
+  import { ModelObj } from 'vue-3d-model'
+
+  export default {
+    name: 'threed-view',
+    components: { ModelObj },
+    props: {
+      objPath: {
+        type: String,
+        required: true
+      },
+      mtlPath: {
+        type: String,
+        required: true
+      }
+    }, 
+    data() {
+      return {
+        loading: true
+     
+      };
+    },
+    methods: {
+      onLoad() {
+        this.loading = false;
+      },
+    }
+  };
+
+  </script>
+  
